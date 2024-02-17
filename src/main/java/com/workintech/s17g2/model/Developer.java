7c3ee6523e797cc40d532f9b45da6c07a5569112
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 21:03:16 +0300

    getter ve setter eklendi

--- src/main/java/com/workintech/s17g2/model/Developer.java
+++ src/main/java/com/workintech/s17g2/model/Developer.java
@@ -12,4 +12,36 @@ public class Developer {
         this.salary = salary;
         this.experience = experience;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Experience getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Experience experience) {
+        this.experience = experience;
+    }
 }
