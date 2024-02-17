02b40c5aa598405d1dc88779fee910fec5a46031
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 21:17:16 +0300

    taxable interface'ini implement eden developertax sinifi olusturuldu methodlar override edildi

---
+++ src/main/java/com/workintech/s17g2/tax/DeveloperTax.java
@@ -0,0 +1,20 @@
+package com.workintech.s17g2.tax;
+
+import org.springframework.stereotype.Component;
+
+public class DeveloperTax implements Taxable{
+    @Override
+    public Double getSimpleTaxRate() {
+        return 15d;
+    }
+
+    @Override
+    public Double getMiddleTaxRate() {
+        return 25d;
+    }
+
+    @Override
+    public Double getUpperTaxRate() {
+        return 35d;
+    }
+}
