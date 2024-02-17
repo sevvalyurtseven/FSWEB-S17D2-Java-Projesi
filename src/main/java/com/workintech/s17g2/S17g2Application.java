415f332b714ad606652591778bfae140fcf50846
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 20:43:03 +0300

    spring initializr ile spring uygulamasi olustu

---
+++ src/main/java/com/workintech/s17g2/S17g2Application.java
@@ -0,0 +1,13 @@
+package com.workintech.s17g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17g2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17g2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/s17g2/S17g2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s17g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17g2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
