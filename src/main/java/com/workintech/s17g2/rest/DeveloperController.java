481f2a7809a971008166b628431c24a75483b8b2
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 23:06:16 +0300

    autowired annotationi ile dependency injection yapildi

--- src/main/java/com/workintech/s17g2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17g2/rest/DeveloperController.java
@@ -1,9 +1,21 @@
 package com.workintech.s17g2.rest;
 
+import com.workintech.s17g2.model.Developer;
+import com.workintech.s17g2.tax.Taxable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/developers")
 public class DeveloperController {
+    private Map<Integer, Developer> developers;
+    private Taxable taxable;
+
+    @Autowired
+    public DeveloperController(Taxable taxable) {
+        this.taxable = taxable;
+    }
 }
