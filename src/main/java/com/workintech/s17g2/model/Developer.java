d742725185f153ba343993482db5a1e4edbc5a46
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 21:02:30 +0300

    constructor tanimlandi

--- src/main/java/com/workintech/s17g2/model/Developer.java
+++ src/main/java/com/workintech/s17g2/model/Developer.java
@@ -5,4 +5,11 @@ public class Developer {
     private String name;
     private Double salary;
     private Experience experience;
+
+    public Developer(Integer id, String name, Double salary, Experience experience) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.experience = experience;
+    }
 }
