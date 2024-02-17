ae84d06fa139a61ba754c60093f6068e1d1bada1
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-17 23:13:50 +0300

    postconstruct annotation'i ile uygulama ayaga kalktıgı anda sadece bir tane developers map objesi yaratilmasini sagladik

--- src/main/java/com/workintech/s17g2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17g2/rest/DeveloperController.java
@@ -2,10 +2,12 @@ package com.workintech.s17g2.rest;
 
 import com.workintech.s17g2.model.Developer;
 import com.workintech.s17g2.tax.Taxable;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -17,5 +19,9 @@ public class DeveloperController {
     @Autowired
     public DeveloperController(Taxable taxable) {
         this.taxable = taxable;
+    }
+    @PostConstruct
+    public void init(){
+        developers = new HashMap<>();
     }
 }
