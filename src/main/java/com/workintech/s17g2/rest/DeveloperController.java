d2137d18ae7ec47a550f9b4348d1eed1bfbb9d17
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-18 18:12:24 +0300

    create islemi güncellendi (post request)

--- src/main/java/com/workintech/s17g2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17g2/rest/DeveloperController.java
@@ -6,12 +6,11 @@ import com.workintech.s17g2.model.DeveloperFactory;
 import com.workintech.s17g2.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -29,13 +28,13 @@ public class DeveloperController {
     public void init(){
         developers = new HashMap<>();
     }
-    //[POST]/workintech/developers => id, name, salary ve experience değerlerini alır, experience tipine bakarak uygun developer objesini oluşturup developers mapine ekler. JuniorDeveloper için salary bilgisinden salarygetSimpleTaxRate() değerini düşmelisiniz. Aynı şekilde MidDeveloper için salarygetMiddleTaxRate(), SeniorDeveloper için salary*getUpperTaxRate() değerlerini salary bilgisinden düşmelisiniz.
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public DeveloperResponse save(@RequestBody Developer developer){
         Developer createdDeveloper = DeveloperFactory.createDeveloper(developer,taxable);
         if(Objects.nonNull(createdDeveloper)){
             developers.put(developer.getId(), developer);
         }
-        return new DeveloperResponse(developer.getId(), developer.getName(), developer.getSalary(), developer.getExperience());
+        return new DeveloperResponse("create successfull", developer.getId(), developer.getName(), developer.getSalary(), developer.getExperience());
     }
 }
