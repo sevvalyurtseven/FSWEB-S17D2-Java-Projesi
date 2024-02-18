57bd96c2368627fc5e72a63b5fff88842fa29caf
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-18 15:05:25 +0300

    post request olusturuldu (create)

--- src/main/java/com/workintech/s17g2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17g2/rest/DeveloperController.java
@@ -1,14 +1,18 @@
 package com.workintech.s17g2.rest;
 
 import com.workintech.s17g2.model.Developer;
+import com.workintech.s17g2.model.DeveloperFactory;
 import com.workintech.s17g2.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/developers")
@@ -23,5 +27,14 @@ public class DeveloperController {
     @PostConstruct
     public void init(){
         developers = new HashMap<>();
+    }
+    //[POST]/workintech/developers => id, name, salary ve experience değerlerini alır, experience tipine bakarak uygun developer objesini oluşturup developers mapine ekler. JuniorDeveloper için salary bilgisinden salarygetSimpleTaxRate() değerini düşmelisiniz. Aynı şekilde MidDeveloper için salarygetMiddleTaxRate(), SeniorDeveloper için salary*getUpperTaxRate() değerlerini salary bilgisinden düşmelisiniz.
+    @PostMapping
+    public Developer save(@RequestBody Developer developer){
+        Developer createdDeveloper = DeveloperFactory.createDeveloper(developer,taxable);
+        if(Objects.nonNull(createdDeveloper)){
+            developers.put(developer.getId(), developer);
+        }
+        return null;
     }
 }
