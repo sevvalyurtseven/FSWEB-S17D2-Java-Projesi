64f0970f4dd8469b05ec5b2f9d77389c9c74f2b7
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-18 17:59:42 +0300

    record sinifina message parametresi eklendi

--- src/main/java/com/workintech/s17g2/dto/DeveloperResponse.java
+++ src/main/java/com/workintech/s17g2/dto/DeveloperResponse.java
@@ -2,5 +2,5 @@ package com.workintech.s17g2.dto;
 
 import com.workintech.s17g2.model.Experience;
 
-public record DeveloperResponse(Integer id, String name, Double salary, Experience experience) {
+public record DeveloperResponse(String message, Integer id, String name, Double salary, Experience experience) {
 }
